88f1c14ca6ad6468c4d88eb07bb78ad59de1e8c8
Afreed <latheefkummath@Afreeds-MacBook-Air.local>
2025-07-21 00:06:04 +0530

    initialized packages

---
+++ app.py
@@ -0,0 +1,16 @@
+#import packages
+import streamlit as st
+from PyPDF2 import PdfReader
+import pandas
+import base64
+import os 
+
+#imports for langchain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.vectorstores import FAISS
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+

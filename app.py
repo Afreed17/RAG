290a8f9e0cd8ff1c77ae1f5f104fb06b2bee18fb
Afreed <latheefkummath@Afreeds-MacBook-Air.local>
2025-07-22 21:58:10 +0530

    set up ebeddings and vector store

--- app.py
+++ app.py
@@ -14,3 +14,27 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 
+from datetime import datetime
+
+
+
+def get_pdf_text(pdfDoc):
+    text = ""
+    for pdf in pdfDoc:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text,model_name):
+    if model_name == "Google AI":
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size = 10000,chunk_overlap = 1000)
+    chunks=text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(model_name,text_chunks,api_key = None):
+    if model_name == "Google AI":
+        embedding = GoogleGenerativeAIEmbeddings(model="models/embedding-001",api_key=api_key)
+    vector_store = FAISS.from_texts(text_chunks,embedding=embedding)
+    vector_store.save_local("faiss_index")
+    return vector_store
